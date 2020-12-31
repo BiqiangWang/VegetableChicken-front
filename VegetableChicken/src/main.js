db92bdf8793ad0000a77f6b9a50265062d040656
LyuJiayao <2667598352@qq.com>
2021-01-01 00:41:01 +0800

    Update

--- VegetableChicken/src/main.js
+++ VegetableChicken/src/main.js
@@ -3,11 +3,22 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
+import AmapVue from '@amap/amap-vue';
+import vueResource from 'vue-resource';
 
+AmapVue.config.version = '2.0'; // 默认2.0，这里可以不修改
+AmapVue.config.key = '84c113b753854d2a372345f043cb50c2';
+AmapVue.config.plugins = [
+    'AMap.ToolBar',
+    'AMap.MoveAnimation',
+    // 在此配置你需要预加载的插件，如果不配置，在使用到的时候会自动异步加载
+];
+Vue.use(AmapVue);
+Vue.use(vueResource);
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
-	router,
+    router,
     render: h => h(App),
 }).$mount('#app')
--- VegetableChicken/src/router/index.js
+++ VegetableChicken/src/router/index.js
@@ -5,42 +5,48 @@ import MyExpress from '../components/MyExpress.vue'
 import Register from '../components/Register.vue'
 import SearchExpress from '../components/SearchExpress.vue'
 import Login from '../components/Login.vue'
+import ExpressInfo from '../components/ExpressInfo.vue'
 
 Vue.use(Router)
 
 const routes = [{
-  path: '/',
-  name: 'Login',
-  component: Login
-},
-{
-  path: '/Register',
-  name: 'Register',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  // component: () =>
-  // import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-  component: Register
-},
-{
-  path: '/MyExpress',
-  name: 'MyExpress',
-  component: MyExpress
-},
-{
-  path: '/SearchExpress',
-  name: 'SearchExpress',
-  component: SearchExpress
-},
-{
-  path: '/Login',
-  name: 'Login',
-  component: Login
-},
+        path: '/',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/Register',
+        name: 'Register',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () =>
+        // import(/* webpackChunkName: "about" */ '../views/About.vue')
+        // }
+        component: Register
+    },
+    {
+        path: '/MyExpress',
+        name: 'MyExpress',
+        component: MyExpress
+    },
+    {
+        path: '/SearchExpress',
+        name: 'SearchExpress',
+        component: SearchExpress
+    },
+    {
+        path: '/Login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/ExpressInfo',
+        name: 'ExpressInfo',
+        component: ExpressInfo
+    },
 ]
 
 export default new Router({
-  routes
+    routes
 })
